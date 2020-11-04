d16328ed54b460f7cbda65f8bccab09a955a2692
Lucas Guimaraes <guimaraes-lucas@outlook.com.br>
2020-11-03 23:04:39 -0300

    feat: add javax validation

---
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
@@ -0,0 +1,33 @@
+package br.com.softblue.bluefood.infrastructure.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.softblue.bluefood.application.ClienteService;
+import br.com.softblue.bluefood.domain.cliente.Cliente;
+
+@Controller
+@RequestMapping(path = "/public")
+public class PublicController {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@GetMapping("/cliente/new")
+	public String newCliente(Model model) {
+		model.addAttribute("cliente", new Cliente());	
+		ControllerHelper.setEditMode(model, false);
+		return "cliente-cadastro";
+	}
+
+	@PostMapping("/cliente/save")
+	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
+		clienteService.saveCliente(cliente);
+		return "cliente-cadastro";
+	}
+}
