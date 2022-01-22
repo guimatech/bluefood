d2ed311ca224b59a4f04328e182fb4c8d313cda8
Lucas Guimarães <guimaraes-lucas@outlook.com.br>
2022-01-22 13:42:20 -0300

    feat: configruando segurança

---
+++ src/main/java/br/com/softblue/bluefood/util/SecurityUtils.java
@@ -0,0 +1,49 @@
+package br.com.softblue.bluefood.util;
+
+import br.com.softblue.bluefood.domain.cliente.Cliente;
+import br.com.softblue.bluefood.domain.restaurante.Restaurante;
+import br.com.softblue.bluefood.infrastructure.web.security.LoggedUser;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtils {
+
+    public static LoggedUser loggedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
+
+        return (LoggedUser) authentication.getPrincipal();
+    }
+
+    public Cliente loggedCliente() {
+        LoggedUser loggedUser = loggedUser();
+
+        if (loggedUser == null) {
+            throw new IllegalStateException("Não existe um usuário logado");
+        }
+
+        if (!(loggedUser.getUsuario() instanceof Cliente)) {
+            throw new IllegalStateException("O usuário logado não é um cliente");
+        }
+
+        return (Cliente) loggedUser.getUsuario();
+    }
+
+    public Restaurante loggedRestaurante() {
+        LoggedUser loggedUser = loggedUser();
+
+        if (loggedUser == null) {
+            throw new IllegalStateException("Não existe um usuário logado");
+        }
+
+        if (!(loggedUser.getUsuario() instanceof Restaurante)) {
+            throw new IllegalStateException("O usuário logado não é um restaurante");
+        }
+
+        return (Restaurante) loggedUser.getUsuario();
+    }
+}
